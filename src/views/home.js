5d4a6845aeec9387c3bfc507b996fdba205628f2
JmsMaupin1 <jmsMaupin1@yahoo.com>
2018-07-22 17:31:28 -0400

    Add Header to home page

--- src/views/home.js
+++ src/views/home.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 // Components
 import Carousel from '../components/carousel';
+import Header from '../components/header';
 
 // Carousel Slide Assets
 import IjoyZenth3Banner from '../assets/carouselslides/ijoy-zenth3-banner.jpg';
@@ -24,10 +25,59 @@ const Slide = styled.div`
 	}
 `;
 
+const Grid = styled.div`
+	padding-top: 50px;
+	width: 80%;
+	margin: 0 auto;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-column-gap: 50px;
+	grid-row-gap: 50px;
+
+	/* Small Laptop */
+	@media only screen and (max-width: 1024px) {
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+
+	/* Tablet */
+	@media only screen and (max-width: 768px) {
+		grid-template-columns: 1fr 1fr;
+		grid-row-gap: 20px;
+		padding-top: 20px;
+	}
+
+	/* Mobile */
+	@media only screen and (max-width: 426px) {
+		width: 100%;
+		grid-template-columns: auto;
+		grid-row-gap: 20px;
+		padding-top: 20px;
+	}
+`;
+
 export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showMenu: false
+		}
+	}
+
+	handleClick = () => {
+		this.setState({showMenu: !this.state.showMenu});
+	}
+
 	render() {
+		const {showMenu} = this.state;
+		
 		return (
 			<div>
+				<Header
+					isOpen={showMenu}
+					mobileMaxWidth="750px"
+					clickHandler={this.handleClick}
+				>
+				</Header>
 				<Carousel>
 					<Slide>
 						<img src={IjoyZenth3Banner} alt=""/>
